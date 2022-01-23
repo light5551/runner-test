f623946b2718a0a52a6d45b8ac8d56b2eb053af0
Sergey Glazunov <31547459+light5551@users.noreply.github.com>
2022-01-23 22:16:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 from github import Github
 
 token = sys.argv[1] 
+number_pr = sys.argv[2]
 print("first print:")
 print(token)
 gh = Github(token)
 repo = gh.get_repo("light5551/runner-test")
-print(repo)
-print()
+pull = repo.get_pull(input_pull_request_num)
+pull_issue = repo.get_issue(pull.number)
+pull_issue.add_to_labels("Github action works fine!")
